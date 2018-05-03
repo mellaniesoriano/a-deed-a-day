a2ed0ca110099213944cd20af4b47059abf5e1b0
mellaniesoriano <23709723+mellaniesoriano@users.noreply.github.com>
2018-05-02 18:49:33 -1000

    things

--- src/config/config.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-  API_KEY: 'AIzaSyBBaLGXXPdWue_zMJ7GLq2YFBj-L0EW3K8',
-  AUTH_DOMAIN: 'a-deed-a-day.firebaseapp.com',
-  DB_URL: 'https://a-deed-a-day.firebaseio.com',
-  PROJECT_ID: 'a-deed-a-day',
-  STORAGE_BUCKET: 'a-deed-a-day.appspot.com',
-  MSG_SENDER_ID: '739887327221'
-};
